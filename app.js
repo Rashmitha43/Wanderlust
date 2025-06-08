5e32e71868d12b02938d73ffb0c7ff80503f3475
Rashmitha Patnana <322103311043@gvpce.ac.in>
2025-06-08 18:58:40 +0530

    Added welcome page

--- app.js
+++ app.js
@@ -46,9 +46,7 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 
 
-// app.get("/",(req,res)=>{
-//     res.send("Hi,I am root");
-// });
+
 
 const store=MongoStore.create({
     mongoUrl:dbUrl,
@@ -95,6 +93,12 @@ app.use((req,res,next)=>{
     res.locals.currUser=req.user;
     next();
 });
+
+
+app.get("/", (req, res) => {
+    res.render("listings/welcome.ejs", { bodyClass: "welcome-page" });
+});
+
 
 
 app.use("/listings",listingRouter);
